7f9fd29a87476ddd67c242d57a41c291a3eb8d46
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2020-12-21 12:49:00 +0000

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#All of the students names printed at Villain Academy
 puts "The students of the Villain Academy"
 puts "----------------------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#Printed a total student count 
 print "Overall, we have "
 print 11
 puts " great students"   
